6d9d9309a83ad93fa0140609c7d004449aa71f28
Ngoc Pham <baongoc@telio.vn>
2022-09-07 10:59:59 +0700

    [ZI-2878] | Initialize Skynet SDK for tracking errors, performances and analysis

---
+++ changelog.md
@@ -0,0 +1,16 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [Unreleased]
+
+## [0.0.1] - 2022-07-09
+
+### Added
+
+- Initialize SDK with basic error tracking and persisting logs to backend service
+- Start writing documentation
+
+### Changed
+
+### Removed
---
+++ config/webpack.prod.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge')
+
+const paths = require('./paths')
+const common = require('./webpack.common')
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: false,
+  output: {
+    path: paths.build,
+    publicPath: '/',
+    filename: 'js/[name].[contenthash].bundle.js',
+  },
+  optimization: {
+    minimize: true,
+    runtimeChunk: {
+      name: 'runtime',
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+})
